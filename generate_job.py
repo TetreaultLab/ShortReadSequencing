52a92c61181cc539e1e4482a5027ab9e4d89afa2
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2024-01-10 14:21:02 -0500

    change numbers

--- generate_job.py
+++ generate_job.py
@@ -3,11 +3,14 @@ import toml
 import os
 
 parser = argparse.ArgumentParser(
-                    prog='GenerateJob',
-                    description='Generate SLURM job for samples based on a TOML project config file.')
+    prog="GenerateJob",
+    description="Generate SLURM job for samples based on a TOML project config file.",
+)
 
-parser.add_argument('--sample', type=str, required=True, help='Sample name.')
-parser.add_argument('--config', type=str, required=True, help='Project config file, including path.')
+parser.add_argument("--sample", type=str, required=True, help="Sample name.")
+parser.add_argument(
+    "--config", type=str, required=True, help="Project config file, including path."
+)
 
 args = parser.parse_args()
 
@@ -19,12 +22,12 @@ sample_name = args.sample
 # LOAD CONFIG FILE
 toml_file = args.config
 
-with open(toml_file, 'r') as f:
+with open(toml_file, "r") as f:
     toml_config = toml.load(f)
 
-email = toml_config['general']['email']
-cores = toml_config['general']['threads']
-memory = toml_config['general']['memory']
+email = toml_config["general"]["email"]
+cores = toml_config["general"]["threads"]
+memory = toml_config["general"]["memory"]
 
 # time
 if toml_config["general"]["sequencing"] == "RNA":
@@ -38,32 +41,34 @@ f.close()
 
 path_config = work_dir + "/" + toml_file
 
-slurm = '''#!/bin/sh
+slurm = """#!/bin/sh
 #SBATCH -N 1 #Number of nodes
 #SBATCH -n {0} # number of cores
 #SBATCH --mem {1}G # memory pool for all cores
 #SBATCH -t {2} # time (DD-HH:MM)
 #SBATCH -o {3}.%N.%j.log
-#SBATCH -e {3}.%N.%j.log
+#SBATCH -e {4}.%N.%j.log
 #SBATCH --mail-type=FAIL
 #SBATCH --account=rrg-tetreaum
-#SBATCH --mail-user={4}
+#SBATCH --mail-user={5}
 #
 ### Load environnment
 #
-module load python/3.10.2 StdEnv/2020 fastqc/0.11.9 bbmap/38.86 star/2.7.9a bwa/0.7.17 gcc/9.3.0 openmpi/4.0.3 salmon/1.4.0 samtools/1.17
+module load python/3.10.2 StdEnv/2020 fastqc/0.11.9 bbmap/38.86 star/2.7.9a bwa-mem2/2.2.1 gcc/9.3.0 openmpi/4.0.3 salmon/1.4.0 samtools/1.17
 source /lustre03/project/6019267/shared/tools/PIPELINES/ShortReadSequencing/bin/activate
 #
 ### Variables
 #
-sample='{3}'
-config='{5}'
+sample='{6}'
+config='{7}'
 #
 ### Launch script
 #
 python /lustre03/project/6019267/shared/tools/PIPELINES/shortReads/pipeline_short_v202402.py --sample ${sample} --config ${config}
 #
-'''.format(cores, memory, time, sample_name, email, path_config)
+""".format(  # noqa: F524
+    cores, memory, time, sample_name, sample_name, email, sample_name, path_config
+)
 
 print(slurm)
-print(slurm,  file=open(work_dir+"/"+sample_name+".slurm", 'w'))
+print(slurm, file=open(work_dir + "/" + sample_name + ".slurm", "w"))
