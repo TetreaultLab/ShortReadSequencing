65483065c60cea274111779ba2f04b98b175fa9e
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2024-01-11 09:22:44 -0500

    finished skeleton

--- pipeline_short_v202402.py
+++ pipeline_short_v202402.py
@@ -27,57 +27,63 @@ def main():
         toml_config = toml.load(f)
 
     # Start of pipeline
-    start_str = "### {}-seq pipeline starting for {} at {} ###".format(
+    start_str = ">>> {}-seq pipeline starting for {} at {}.".format(
         toml_config["general"]["sequencing"], sample, start
     )
-    print("#" * len(start_str) + "\n" + start_str + "\n" + "#" * len(start_str))
+    print("=" * len(start_str) + "\n" + start_str + "\n" + "=" * len(start_str))
 
     # Get tools and versions
     dict_keys = toml_config.keys()
 
     function_queue = []
+    print(">>> Parameters:")
     # Quality control
     vqc = subprocess.check_output(["cat", "../version_fastqc_N.txt"], text=True).strip()
-    print(f"Quality control: FastQC ({vqc})")
+    print(f"\t>>> Quality control: FastQC ({vqc})")
+    function_queue.append(fastqc)
 
     # Trimming
     if "bbduk" in dict_keys:
         vt = subprocess.check_output(
             ["cat", "../version_bbduk_N.txt"], text=True
         ).strip()
-        print(f"Trimming: BBDuk (v{vt})")
+        print(f"\t>>> Trimming: BBDuk (v{vt})")
         function_queue.append(bbduk)
     else:
-        print("Trimming: none")
+        print("\t>>> Trimming: none")
 
     # Alignment
     if "star" in dict_keys:
         va = subprocess.check_output(
             ["cat", "../version_star_N.txt"], text=True
         ).strip()
-        print(f"Alignment: STAR (v{va})")
+        print(f"\t>>> Alignment: STAR (v{va})")
+        function_queue.append(star)
     elif "bwa" in dict_keys:
         va = subprocess.check_output(
             ["cat", "../version_bwa-mem2_N.txt"], text=True
         ).strip()
-        print(f"Alignment: BWA-MEM2 (v{va})")
+        print(f"\t>>> Alignment: BWA-MEM2 (v{va})")
+        function_queue.append(bwa)
     else:
-        print("Alignment: none")
+        print("\t>>> Alignment: none")
 
     # Pseudoalignment
     if "salmon" in dict_keys:
         vp = subprocess.check_output(
             ["cat", "../version_salmon_N.txt"], text=True
         ).strip()
-        print(f"Pseudoalignment: Salmon (v{vp})")
+        print(f"\t>>> Pseudoalignment: Salmon (v{vp})")
+        function_queue.append(salmon)
     else:
-        print("Pseudoalignment: none")
+        print("\t>>> Pseudoalignment: none")
 
     # Sorting and indexing
     vsi = subprocess.check_output(
         ["cat", "../version_samtools_N.txt"], text=True
     ).strip()
-    print(f"Sorting/Indexing: Samtools (v{vsi})")
+    print(f"\t>>> Sorting/Indexing: Samtools (v{vsi})")
+    function_queue.append(samtools)
 
     # MarkDuplicates
     vgatk = subprocess.check_output(["cat", "../version_gatk_N.txt"], text=True).strip()
@@ -85,35 +91,29 @@ def main():
     vpic = subprocess.check_output(
         ["cat", "../version_picard_N.txt"], text=True
     ).strip()
-    print(f"MarkDuplicates: GATK ({vgatk}) & Picard (v{vpic})")
+    print(f"\t>>> MarkDuplicates: GATK ({vgatk}) & Picard (v{vpic})")
+    function_queue.append(markduplicates)
 
     # Quantification
     if "featurecounts" in dict_keys:
         vq = subprocess.check_output(
             ["cat", "../version_featurecounts_N.txt"], text=True
         ).strip()
-        print(f"Quantification: featureCounts ({vq})")
+        print(f"\t>>> Quantification: featureCounts ({vq})")
+        function_queue.append(featurecounts)
     else:
-        print("Quantification: none")
+        print("\t>>> Quantification: none")
 
-    # Calling initial quality control
-    fastqc()
+    # MultiQC
+    vmqc = subprocess.check_output(
+        ["cat", "../version_multiqc_N.txt"], text=True
+    ).strip()
+    print(f"\t>>> Quality control report: multiQC (v{vmqc})")
+    function_queue.append(multiqc)
 
     # Calling each steps
     for func in function_queue:
-        func(toml_config)
-    # Steps
-    # 01. Quality control
-    # 02. Trimming
-    # 03. Quality control
-    # 04. Alignment
-    # 05. Quality control
-    # 06. Pseudoalignment
-    # 07. Quality control
-    # 08. Sorting/indexing
-    # 09. If PE: markduplicates
-    # 10. Quantification
-
+        func(sample, toml_config)
     # reads = toml_config['general']['reads']
     # fastq = toml_config['general']['fastq']
 
@@ -125,10 +125,10 @@ def main():
 
     end = get_time()
     total_time = end - start
-    end_str = "### {}-seq pipeline for {} completed in {} ###".format(
+    end_str = ">>> {}-seq pipeline for {} completed in {}.".format(
         toml_config["general"]["sequencing"], sample, total_time
     )
-    print("#" * len(end_str) + "\n" + end_str + "\n" + "#" * len(end_str))
+    print("=" * len(end_str) + "\n" + end_str + "\n" + "=" * len(end_str))
 
 
 def get_time():
@@ -144,14 +144,42 @@ def get_file_names():
     print()
 
 
-def fastqc():
+def fastqc(sample, toml_config):
     title("FastQC")
 
 
-def bbduk(toml_config):
+def bbduk(sample, toml_config):
     title("BBDuk")
     # print(toml_config["bbduk"])
     # subprocess.call(["bbduk.sh", "--version"])
+
+
+def star(sample, toml_config):
+    title("STAR")
+
+
+def bwa(sample, toml_config):
+    title("BWA-MEM2")
+
+
+def salmon(sample, toml_config):
+    title("Salmon")
+
+
+def samtools(sample, toml_config):
+    title("Samtools")
+
+
+def markduplicates(sample, toml_config):
+    title("MarkDuplicates")
+
+
+def featurecounts(sample, toml_config):
+    title("FeatureCounts")
+
+
+def multiqc(sample, toml_config):
+    title("MutliQC")
 
 
 if __name__ == "__main__":
