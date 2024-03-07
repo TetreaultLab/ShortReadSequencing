af161859e615f3908d759f13a14e4e863312d721
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2024-03-07 10:32:33 -0500

    added script to get gene association between gene id, gene name and biotype

---
+++ gene_association.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+name = "Homo_sapiens.GRCh38.105"
+lines = []
+with open("/lustre03/project/6019267/shared/tools/PIPELINES/References/"+name+".gtf", "r") as f:
+    for line in f:
+        line = line.strip()
+        if not line.startswith('#'):
+            line_strip = line.split("\t")
+            matchers = ['gene_id']
+            matching = [s for s in line_strip if any(xs in s for xs in matchers)]
+            line_gene = matching[0].split(";")
+            myList = [i.strip().split(' ') for i in line_gene]
+            matchers2 = ['gene_id', 'gene_name', 'gene_biotype']
+            final = [s for s in myList if any(xs in s for xs in matchers2)]
+            final = [i[1].replace('"', '') for i in final]
+            #print(final)
+            if len(final) == 3:
+                lines.append(final)
+
+df = pd.DataFrame(lines, columns=['gene_id', 'gene_name', 'gene_biotype'])
+df = df.drop_duplicates()
+df.to_csv(name+"_gene_association.txt", sep="\t", index=False)
+
