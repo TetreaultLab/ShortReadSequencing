4e4019768d216572bb6fb784b61268a9e7a7c37a
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2025-09-12 10:15:11 -0400

    remove temporary

--- pipeline_short.py
+++ pipeline_short.py
@@ -403,6 +403,7 @@ def star(sample, toml_config):
     subprocess.run(["mkdir", "-p", output])
 
     temporary = toml_config["general"]["temporary"] + "/" + sample + "/star_tmp"
+    subprocess.run(["rm", "-r", temporary])
 
     ref = get_reference(toml_config["general"]["reference"], "star")["index"] + "/"
 
