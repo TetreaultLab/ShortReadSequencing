f7eadc6c7ff6eca28bc49cdfd2e1c92f5a4ce117
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2025-09-23 09:47:19 -0400

    quantification

--- generate_config.py
+++ generate_config.py
@@ -42,7 +42,7 @@ args = parser.parse_args()
 # if nothing is passed, defaults to trim, quantification and variant calling
 if not any([args.trimming, args.pseudo, args.quantification, args.variants]):
     args.trim = True
-    args.quant = True
+    args.quantification = True
     args.variants = True
 
 work_dir = os.getcwd()
