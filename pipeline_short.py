a4c321f5d5e63dbc40651e80c045c01ea860631c
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2025-10-17 09:12:10 -0400

    rsync changes

--- pipeline_short.py
+++ pipeline_short.py
@@ -178,7 +178,7 @@ def main():
     results = Path(str(current_directory).replace("/work", "/results/"))
     output = Path(toml_config["general"]["output"] + "/" + sample)
     results.parent.mkdir(parents=True, exist_ok=True)
-    cmd = ["rsync", "-avzh", "--no-g", "--no-p", "--partial", str(output), str(results)]
+    cmd = ["rsync", "-avxH", "--no-g", "--no-p", "--partial", str(output), str(results)]
     subprocess.run(cmd, check=True)
     final = f"{str(results)}/{sample}"
     print(f"\n\n>>> Results found in {final}")
