8e71a3d6edf41fbf205ba9b8bf036cbc97f674ed
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2025-12-10 13:07:02 -0500

    env -i

--- run_openCravat.bash
+++ run_openCravat.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+env -i PATH=/lustre09/project/6019267/shared/tools/variants/annotation/openCravat_env/bin:/usr/bin:/bin
 module --force purge
 module load StdEnv/2023 python/3.10
 source /lustre09/project/6019267/shared/tools/variants/annotation/openCravat_env/bin/activate
