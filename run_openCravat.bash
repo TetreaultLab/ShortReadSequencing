5dc66c563686eda3f90ef43c9cd994bd755ad055
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2025-12-10 11:56:42 -0500

    force purge

--- run_openCravat.bash
+++ run_openCravat.bash
@@ -1,6 +1,6 @@
-module purge
-module load python/3.10
+module --force purge
+module load StdEnv/2023 python/3.10
 source /lustre09/project/6019267/shared/tools/variants/annotation/openCravat_env/bin/activate
 
-# Run your job
+# Run openCravat
 {0}
