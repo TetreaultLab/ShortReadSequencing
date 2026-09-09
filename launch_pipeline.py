b30ac87bfe6f8eda976e7fc66f51eb3d1bc5b93a
Marjorie Labrecque <labrecque.marjorie@gmail.com>
2026-09-09 15:48:59 -0400

    temporary star fix

--- launch_pipeline.py
+++ launch_pipeline.py
@@ -616,8 +616,8 @@ def star(sample, toml_config, done):
     output = toml_config["general"]["output"] + "/" + sample + "/Aligned"
     subprocess.run(["mkdir", "-p", output])
 
-    temporary = Path(toml_config["general"]["tmp"]) / sample / "star_tmp"
-    if temporary.is_dir():
+    temporary = f"{toml_config['general']['tmp']}/{sample}/star_tmp"
+    if Path(temporary).is_dir():
         subprocess.run(["rm", "-r", str(temporary)])
 
     ref = get_reference(toml_config["general"]["reference"], "star")["index"]
